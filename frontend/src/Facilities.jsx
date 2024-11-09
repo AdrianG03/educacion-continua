5cf66e3c6ecd91439ce20c3f64be2bbc5571350e
AdrianG <guevadev@gmail.com>
2024-11-08 19:51:51 -0600

    images facilities

--- frontend/src/Facilities.jsx
+++ frontend/src/Facilities.jsx
@@ -1,64 +1,48 @@
-import teck2 from "./assets/facilitiesAssets/template.jpg";
-import facilities from "./data/laboratories.json";
+import { useState } from "react";
+import Laboratories from "./facilitiesComponents/Laboratories";
+import Rooms from "./facilitiesComponents/Rooms";
 
 export default function Facilities() {
+  const [selectedSection, setSelectedSection] = useState("laboratories");
+
+  const handleSectionChange = (section) => {
+    setSelectedSection(section);
+  };
+
   return (
     <div className="pt-10 h-fit w-screen gap-1 flex justify-center">
       <div className="w-3/4">
         <h1 className="text-3xl font-bold">NUESTRAS INSTALACIONES</h1>
         <p className="text-xl font-medium break-words text-justify">
-          El Tecnológico Nacional de México en Cuautla cuenta con mútiples
+          El Tecnológico Nacional de México en Cuautla cuenta con múltiples
           espacios en el campus universitario para el desarrollo de actividades
           en diferentes áreas, desde laboratorios de cómputo, espacios de uso
           múltiples, salas ejecutivas, salas audiovisuales, entre muchas otras.
-          Todos acondicionados para brindar una experiendia de aprendizaje de
+          Todos acondicionados para brindar una experiencia de aprendizaje de
           calidad.
         </p>
         <div className="flex justify-between mt-10 gap-6">
-          <button className="flex-grow rounded-lg bg-primary text-white font-bold text-xl hover:bg-blue-800 h-10">
+          <button
+            onClick={() => handleSectionChange("laboratories")}
+            className={`flex-grow rounded-lg bg-primary text-white font-bold text-xl hover:bg-blue-800 h-10 ${
+              selectedSection === "laboratories" ? "bg-blue-800" : ""
+            }`}
+          >
             LABORATORIOS
           </button>
-          <button className="flex-grow rounded-lg bg-primary text-white font-bold text-xl hover:bg-blue-800 h-10">
+          <button
+            onClick={() => handleSectionChange("rooms")}
+            className={`flex-grow rounded-lg bg-primary text-white font-bold text-xl hover:bg-blue-800 h-10 ${
+              selectedSection === "rooms" ? "bg-blue-800" : ""
+            }`}
+          >
             SALAS
           </button>
         </div>
 
-        <div className="flex justify-center my-10">
-          <h1 className="text-2xl font-bold">LABORATORIOS</h1>
+        <div className="mt-10">
+          {selectedSection === "laboratories" ? <Laboratories /> : <Rooms />}
         </div>
-        {facilities.map((fac) => (
-          <>
-            <div className="p-4 mb-10 border rounded-lg shadow-lg bg-white text-black text-justify flex gap-4 items-center">
-              {/* Recuadro de la imagen */}
-              <div className="w-auto h-auto flex-shrink-0 border rounded-lg overflow-hidden bg-gray-200">
-                <img
-                  src={teck2}
-                  alt="Laboratorio"
-                  className="w-[570px] h-[250px] object-cover"
-                />
-              </div>
-              {/* Información del curso */}
-              <div>
-                <h2 className="text-xl font-semibold mb-2">{fac.name}</h2>
-                <p className="text-gray-600">
-                  <b>Ubicación:</b> {fac.ubication}
-                </p>
-                <p className="text-gray-600">
-                  <b>Capacidad:</b> {fac.capacity}
-                </p>
-                <p className="text-gray-600">
-                  <b>Tipo de servicios:</b> {fac.services}
-                </p>
-                <p className="text-gray-600">
-                  <b>Equipo:</b> {fac.equipment}
-                </p>
-                <p className="text-gray-600">
-                  <b>Descripción:</b> {fac.description}
-                </p>
-              </div>
-            </div>
-          </>
-        ))}
       </div>
     </div>
   );
---
+++ frontend/src/facilitiesComponents/Laboratories.jsx
@@ -0,0 +1,92 @@
+import facilities from "../data/laboratories.json";
+import teck2 from "../assets/laboAssets/template.jpg";
+import img0 from "../assets/laboAssets/img0.jpg";
+import img1 from "../assets/laboAssets/img1.jpg";
+import img2 from "../assets/laboAssets/img2.jpg";
+import img3 from "../assets/laboAssets/img3.jpg";
+import img4 from "../assets/laboAssets/img4.jpg";
+import img5 from "../assets/laboAssets/img5.jpg";
+import img6 from "../assets/laboAssets/template.jpg";
+import img7 from "../assets/laboAssets/img7.jpg";
+import img8 from "../assets/laboAssets/template.jpg";
+import img9 from "../assets/laboAssets/template.jpg";
+import img10 from "../assets/laboAssets/template.jpg";
+import img11 from "../assets/laboAssets/template.jpg";
+import img12 from "../assets/laboAssets/template.jpg";
+import img13 from "../assets/laboAssets/template.jpg";
+import img14 from "../assets/laboAssets/template.jpg";
+import img15 from "../assets/laboAssets/template.jpg";
+import img16 from "../assets/laboAssets/img16.jpg";
+import img17 from "../assets/laboAssets/img17.jpg";
+import img18 from "../assets/laboAssets/img18.jpg";
+import img19 from "../assets/laboAssets/template.jpg";
+import img20 from "../assets/laboAssets/template.jpg";
+import img21 from "../assets/laboAssets/template.jpg";
+
+export default function Laboratories() {
+  const images = [
+    img0,
+    img1,
+    img2,
+    img3,
+    img4,
+    img5,
+    img6,
+    img7,
+    img8,
+    img9,
+    img10,
+    img11,
+    img12,
+    img13,
+    img14,
+    img15,
+    img16,
+    img17,
+    img18,
+    img19,
+    img20,
+    img21,
+    // Agrega las imágenes en el mismo orden que sus índices
+  ];
+  return (
+    <>
+      <div className="flex justify-center my-10">
+        <h1 className="text-2xl font-bold">LABORATORIOS</h1>
+      </div>
+      {facilities.map((fac) => (
+        <>
+          <div className="p-4 mb-10 border rounded-lg shadow-lg bg-white text-black text-justify flex gap-4 items-center">
+            {/* Recuadro de la imagen */}
+            <div className="w-auto h-auto flex-shrink-0 border rounded-lg overflow-hidden bg-gray-200">
+              <img
+                src={images[fac.imgIndex] || teck2}
+                alt={fac.name}
+                className="w-[570px] h-[250px] object-cover"
+              />
+            </div>
+            {/* Información del curso */}
+            <div>
+              <h2 className="text-xl font-semibold mb-2">{fac.name}</h2>
+              <p className="text-black">
+                <b>Ubicación:</b> {fac.ubication}
+              </p>
+              <p className="text-black">
+                <b>Capacidad:</b> {fac.capacity}
+              </p>
+              <p className="text-black">
+                <b>Tipo de servicios:</b> {fac.services}
+              </p>
+              <p className="text-black">
+                <b>Equipo:</b> {fac.equipment}
+              </p>
+              <p className="text-black">
+                <b>Descripción:</b> {fac.description}
+              </p>
+            </div>
+          </div>
+        </>
+      ))}
+    </>
+  );
+}
