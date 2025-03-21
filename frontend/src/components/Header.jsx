e079059ea7d3d6607cb817c50cabe6b03430cf9b
AdrianG <guevadev@gmail.com>
2025-03-21 00:34:51 -0600

    last change to deploy

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -47,7 +47,7 @@ export default function Header() {
         >
           <li>
             <Link
-              to="/"
+              to="/educacion-continua"
               className="hover:underline block py-2 md:py-0 px-6 md:px-0"
             >
               INICIO
